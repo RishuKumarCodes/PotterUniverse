d5583a1c57b56f7ea30fe0073c472efd6d04ec12
Rishu Kumar <rishukumar9233@gmail.com>
2024-08-27 08:31:48 +0530

    improved home page styling, added music box

--- PotteraApp/Home/home.js
+++ PotteraApp/Home/home.js
@@ -1,7 +1,22 @@
+const audio = new Audio('../Assets/music/potter.mp3');
+const musicBox = document.querySelector(".musicbox");
+const boxclick = new Audio('../Assets/music/boxclick.mp3');
+boxclick.volume = 0.3;
+const boximg = document.querySelector(".box");
 
-window.addEventListener("load", () => {
-    const audio = document.getElementById("background_music");
-    audio.play().catch((error) => {
-      console.log("There was a error playing the music");
-    });
-  });
+function toggleMusic() {
+  if (audio.paused) {
+    audio.play();
+    boximg.innerHTML = `<img src="../Assets/image/musicboxopen.png" alt="">`
+    musicBox.classList.remove('animate_box');
+    document.getElementById('playButton').innerText = 'Pause Music';
+  } else {
+    boximg.innerHTML = `<img src="../Assets/image/musicboxclosed.png" alt="">`
+    audio.pause();
+    musicBox.classList.add('animate_box');
+  }
+}
+document.querySelector('.musicbox').addEventListener('click', () => {
+  boxclick.play();
+  toggleMusic();
+});
