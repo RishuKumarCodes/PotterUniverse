00e8fba8440af0ccb4127861594dc6fc73ec5967
amandollar <toxicempire3@gmail.com>
2024-08-31 23:10:03 +0530

    created a virtual db for non fetching data

---
+++ Welcome_page/Books/books_data.js
@@ -0,0 +1,183 @@
+async function getChapterSummary(bookSlug, chapterId) {
+    const bookChaptersData = {
+        bookdata: [
+
+
+            //Book 1
+
+            {
+                bookslug: "harry-potter-and-the-chamber-of-secrets",
+                chapterdata: [
+
+                    //Chapter 1
+
+                    {
+                        chapterid: "afff3a3b-3c0f-422b-8652-14454d1c63c9",
+                        summary: `In the first chapter of "Harry Potter and the Chamber of Secrets," we find Harry back at the Dursleys' home for the summer. It's his twelfth birthday, but no one in the household acknowledges it. Instead, Harry is mistreated as usual, doing chores and being ignored by his aunt, uncle, and cousin Dudley. The Dursleys are more focused on preparing for an important dinner with a potential business partner, Mr. Mason. Harry is warned to stay out of sight and not to mention anything about magic, as the Dursleys are still deeply uncomfortable with his magical abilities.The chapter sets the tone for Harry's miserable summer at the Dursleys', highlighting the stark contrast between his life at Hogwarts and the non-magical world he has to endure during the holidays.`,
+                    },
+
+
+                    //Chapter 2
+
+                    {
+                        chapterid: "dbe230c2-7e08-4e10-b3a0-7bb259d97d3d",
+                        summary: `In Chapter 2 of "Harry Potter and the Chamber of Secrets," Harry is surprised by an unexpected visitor in his room—Dobby, a house-elf. Dobby warns Harry not to return to Hogwarts because terrible things are going to happen there. Despite Dobby's pleas, Harry refuses to stay away from school, as Hogwarts is the only place where he feels truly at home.Desperate to prevent Harry from returning, Dobby admits to intercepting Harry's letters from his friends, hoping Harry would think they forgot about him and decide not to go back to Hogwarts. When Harry insists on going back, Dobby causes chaos by making a dessert float in midair and then drop in the kitchen, ruining the Dursleys' dinner party. This leads to Harry getting into trouble with Uncle Vernon, who locks him in his room and bars the window.The chapter ends with Harry feeling trapped and hopeless, unable to communicate with his friends or escape the Dursleys' house.`
+                    },
+
+
+                    //Chapter 3
+
+                    {
+                        chapterid: "4b65108f-0e1c-41d5-8838-7b8a8cae2eac",
+                        summary: `In Chapter 3 of "Harry Potter and the Chamber of Secrets," Harry is rescued from the Dursleys by Ron Weasley and his brothers, Fred and George, who arrive in a flying Ford Anglia. They break Harry out of his locked room and take him to their home, The Burrow. The Burrow is a cozy, magical house that is very different from the Dursleys' home. It's full of warmth, laughter, and magical items. Harry is amazed and delighted by everything he sees, from the self-knitting needles to the clock that shows where each member of the family is at any given time. Mrs. Weasley is angry with her sons for taking the car but is kind to Harry, making sure he feels welcome.Harry spends the next few days enjoying his time at The Burrow, helping the Weasleys with chores and experiencing a true magical family life. He also reunites with Ginny, Ron's younger sister, who is shy and awkward around him. The chapter highlights the stark contrast between the loving, chaotic environment of the Weasleys' home and the cold, oppressive atmosphere of the Dursleys'. Harry feels truly at home for the first time since leaving Hogwarts.`
+                    },
+
+
+
+                    //Chapter 4
+
+                    {
+                        chapterid: "b171974a-39e5-424f-976c-443529f2bcee",
+                        summary: `In Chapter 4 of "Harry Potter and the Chamber of Secrets," Harry and the Weasleys travel to Diagon Alley to buy school supplies for the upcoming year at Hogwarts. They use Floo Powder to travel, but Harry accidentally ends up in Knockturn Alley, a dark and creepy part of the wizarding shopping district known for its shady shops and sinister customers.Luckily, Harry is found by Hagrid, who helps him return to Diagon Alley, where he reunites with the Weasleys. They head to Flourish and Blotts, the bookstore, where they meet Hermione and her parents. The bookstore is crowded because Gilderoy Lockhart, a famous and flamboyant wizard author, is there signing his latest book. Lockhart recognizes Harry and seizes the opportunity to announce that he will be the new Defense Against the Dark Arts teacher at Hogwarts.During the event, there’s a confrontation between Lucius Malfoy and Arthur Weasley. Lucius insults the Weasleys for their poverty and taunts Harry about his fame. A scuffle breaks out, and in the chaos, Lucius discreetly slips an old, tattered book into Ginny Weasley's cauldron without anyone noticing.The chapter ends with the group leaving Diagon Alley, excited for the new school year, though the encounter with the Malfoys leaves an uneasy feeling.`
+                    },
+
+
+
+                    //Chapter 5
+
+                    {
+                        chapterid: "72a0ff2f-5a48-408d-8263-b75f99154bca",
+                        summary: `In Chapter 5 of "Harry Potter and the Chamber of Secrets," Harry and the Weasleys prepare to return to Hogwarts. At King's Cross Station, the family rushes to Platform 9¾, but when it's Harry and Ron's turn to go through the barrier, it mysteriously seals, preventing them from accessing the platform. Unable to board the Hogwarts Express, Harry and Ron decide to take the flying Ford Anglia to get to school.The journey is thrilling at first, as they fly above the clouds and eventually spot the Hogwarts Express below them. However, the car begins to malfunction as they approach Hogwarts. It loses power, and they crash into the Whomping Willow, a violent, magical tree on the school grounds. The tree attacks the car, damaging it severely.After the chaotic landing, the car ejects them and their luggage before driving off into the Forbidden Forest. Harry and Ron are left to face the consequences as they are caught by Professor Snape, who is furious with them. They are taken to Professor McGonagall and eventually to Professor Dumbledore. Though Dumbledore is disappointed, he doesn't expel them, but they receive stern warnings and detentions.The chapter ends with Harry and Ron feeling relieved that they weren’t expelled but worried about how the rest of the school year will go after such a rough start.`
+                    },
+
+                    //Chapter 6
+
+                    {
+                        chapterid: "294ceac4-6f1c-4e7d-a77d-b33b6217fdc2",
+                        summary: `In Chapter 6 of "Harry Potter and the Chamber of Secrets," Harry and Ron start their school year at Hogwarts with a lot of attention—mostly negative—due to their dramatic arrival in the flying car. They quickly fall back into the rhythm of school life, attending classes and catching up with friends.One of their first classes is Defense Against the Dark Arts, taught by the new, self-obsessed professor, Gilderoy Lockhart. Lockhart is famous for his books and supposed heroic deeds, but it's clear he loves the spotlight more than teaching. During the class, Lockhart releases a cage of Cornish pixies, mischievous magical creatures, to "teach" the students how to handle them. However, he quickly loses control, and the pixies cause chaos in the classroom. Lockhart is unable to handle the situation, and it’s left to Hermione, Harry, and Ron to contain the pixies after Lockhart flees in panic.After the disastrous lesson, Harry and his friends start to realize that Lockhart might not be as competent as he claims. The rest of the chapter follows Harry as he continues to settle into his classes, noticing that Ginny Weasley seems to be struggling to adjust to her first year at Hogwarts.The chapter ends with Harry receiving unwanted attention from Lockhart, who seems eager to associate himself with Harry's fame, much to Harry’s embarrassment and annoyance.`
+                    },
+
+
+                    //Chapter 7
+
+                    {
+                        chapterid: "eabdc1e8-c678-4248-9d2d-ce9f79994bb1",
+                        summary: `In Chapter 7 of "Harry Potter and the Chamber of Secrets," Harry begins his second year at Hogwarts by joining the Gryffindor Quidditch team for practice. During a practice session, they are interrupted by the Slytherin team, who claim they have special permission from Professor Snape to use the pitch. Slytherin has a new Seeker this year—Draco Malfoy—who has been given a Nimbus 2001 broomstick, the latest model, courtesy of his wealthy father.An argument breaks out between the Gryffindors and Slytherins. Draco Malfoy insults Hermione by calling her a "Mudblood," a derogatory term for Muggle-born witches and wizards. Ron is furious and tries to hex Draco, but his broken wand backfires, causing Ron to vomit slugs uncontrollably. Harry and Hermione help Ron to Hagrid's hut, where Hagrid explains the meaning of the word "Mudblood" and reassures Hermione that her magical abilities are just as strong as anyone else's, despite her non-magical parentage.Meanwhile, the trio notices that Harry is hearing strange, disembodied voices that no one else seems to hear. This is unsettling for Harry, but he keeps it to himself for now. The chapter hints at something dark and dangerous looming over Hogwarts, adding to the growing tension.The chapter ends with the trio leaving Hagrid's hut, still disturbed by the day's events and the increasing unease at Hogwarts.`
+                    },
+
+                    //Chapter 8
+                    {
+                        chapterid: "e5183c7f-d559-4b9b-8bd8-bfd65475077d",
+                        summary: `In Chapter 8 of "Harry Potter and the Chamber of Secrets," Harry receives an unusual invitation from Nearly Headless Nick, the Gryffindor House ghost. Nick invites Harry, Ron, and Hermione to his 500th Deathday Party, a celebration marking the anniversary of his death. Harry agrees to attend out of curiosity and to avoid an annoying Halloween feast at Hogwarts. The trio arrives at the Deathday Party, which is held in a cold, dark dungeon filled with hundreds of ghosts. The atmosphere is eerie, with rotten food and a haunting, off-key music band. The party is quite different from the lively events the students are used to, and the three friends quickly realize that it's not as enjoyable as they had hoped. They meet several ghosts, including Moaning Myrtle, who haunts a girls' bathroom at Hogwarts. As they try to politely endure the party, the trio overhears a conversation between Nearly Headless Nick and the Headless Hunt, a group of more popular ghosts who exclude Nick because his head is not completely severed. The interaction adds to the somber tone of the event, making the trio even more uncomfortable. As they leave the party, Harry hears the strange, disembodied voice again. This time, it's clearer and seems to be saying, "Rip... tear... kill..." The voice leads Harry, Ron, and Hermione to a corridor, where they find a horrifying scene: Mrs. Norris, the caretaker Filch's cat, is found petrified, hanging by her tail from a torch bracket. Nearby, a chilling message is written on the wall in blood: "The Chamber of Secrets has been opened. Enemies of the heir, beware." The chapter ends with the trio standing in shock as other students and staff arrive at the scene, setting the stage for the mystery of the Chamber of Secrets to deepen.`
+                    },
+
+                    //Chapter 9
+                    {
+                        chapterid: "80ed09ce-d337-40b5-a14c-9deecc11878d",
+                        summary: `In Chapter 9 of "Harry Potter and the Chamber of Secrets," Harry and Ron discover that their flying car crash has had severe consequences. The Weasleys' car is gone, having run away to the Forbidden Forest, and they face detention for their actions. During detention, they are joined by Draco Malfoy and his cronies, who are also serving time. The detention takes place in the dark, eerie Forbidden Forest, where Hagrid leads the students on a dangerous mission to search for an injured creature. They encounter a large, mysterious creature that seems to be stalking them. Hagrid’s giant spider, Aragog, is revealed to be the creature behind the recent attacks. Aragog shares that he knows nothing about the attacks but hints that the Chamber of Secrets is linked to the dark past of Hogwarts. The chapter ends with the students returning to the castle, more determined than ever to uncover the mystery of the Chamber of Secrets.`
+                    },
+
+                    //Chapter 10
+                    {
+                        chapterid: "2484bcec-3a4e-458e-8ebf-99bf02fe28a1",
+                        summary: `In Chapter 10 of "Harry Potter and the Chamber of Secrets," Harry and Ron are deeply troubled by the recent events and the petrification of Mrs. Norris. They decide to investigate the mysterious voices Harry has been hearing. Meanwhile, the students are facing increasing fear as more attacks occur. Harry, Ron, and Hermione attempt to find clues in the library and learn more about the legend of the Chamber of Secrets. Hermione gets petrified, leaving Harry and Ron to continue their investigation on their own. They discover that Hermione had been researching a book about magical creatures that might be linked to the attacks. The chapter ends with Harry and Ron feeling more determined to solve the mystery, despite the mounting danger and obstacles.`
+                    },
+
+
+                 ],
+            },
+            {
+                bookslug: 2,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 2, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 2, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 2, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 3,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 3, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 3, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 3, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 4,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 4, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 4, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 4, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 5,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 5, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 5, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 5, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 6,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 6, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 6, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 6, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 7,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 7, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 7, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 7, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 8,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 8, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 8, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 8, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 9,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 9, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 9, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 9, chapter 3." },
+                ],
+            },
+            {
+                bookslug: 10,
+                chapterdata: [
+                    { chapterid: 1, summary: "Summary of book 10, chapter 1." },
+                    { chapterid: 2, summary: "Summary of book 10, chapter 2." },
+                    { chapterid: 3, summary: "Summary of book 10, chapter 3." },
+                ],
+            },
+        ],
+    };
+
+    // Find the book with the matching bookSlug
+    const book = await bookChaptersData.bookdata.find(
+        (b) => b.bookslug === bookSlug
+    );
+    // If the book is found, find the chapter with the matching chapterId
+    if (book) {
+        const chapter = await book.chapterdata.find(
+            (c) => c.chapterid === chapterId
+        );
+
+        // If the chapter is found, return the summary
+        if (chapter) {
+            return chapter.summary;
+        } else {
+            return `Coming Soon`;
+        }
+    } else {
+        return `Book ${bookSlug} not found.`;
+    }
+}
